e992181802ad9bb8bfb48ecb731e71f0c07bbc6d
Tsepushtanov Anatoliy <tsepushtanov@outlook.com>
2022-08-18 16:45:10 +0500

    1. Принципы Kiss, Dry и Yagni [#238813]

---
+++ src/test/java/ru/job4j/ood/kiss/MaxMinTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.ood.kiss;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class MaxMinTest {
+
+    @Test
+    public void whenMax() {
+        List<Integer> rsl = List.of(1, 2, 3, 4, 5);
+        Comparator<Integer> comparator = Integer::compareTo;
+        assertThat(new MaxMin().max(rsl, comparator), is(5));
+    }
+
+    @Test
+    public void whenMin() {
+        List<Integer> rsl = List.of(1, 2, 3, 4, 5);
+        Comparator<Integer> comparator = Integer::compareTo;
+        assertThat(new MaxMin().min(rsl, comparator), is(1));
+    }
+}
