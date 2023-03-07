084297a0e5c8ef7b0b221ec36a89f2897e1f68cb
Tsepushtanov Anatoliy <tsepushtanov@outlook.com>
2023-03-07 14:22:25 +0500

    1. Динамическое перераспределение продуктов [#854]

--- src/main/java/ru/job4j/ood/solid/lsp/foodstorage/ControlQuality.java
+++ src/main/java/ru/job4j/ood/solid/lsp/foodstorage/ControlQuality.java
@@ -1,5 +1,6 @@
 package ru.job4j.ood.solid.lsp.foodstorage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ControlQuality {
@@ -13,6 +14,17 @@ public class ControlQuality {
     public void add(Food food) {
         for (Store store : stores) {
             store.add(food);
+        }
+    }
+
+    public void resort() {
+        List<Food> foods = new ArrayList<>();
+        for (Store store : stores) {
+            foods.addAll(store.showAll());
+            stores.clear();
+        }
+        for (Food food : foods) {
+            add(food);
         }
     }
 }
